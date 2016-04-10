6e83750a86c87a4d1ea027da5a6ff827ec9b44e4
Jake Secor <jake@jakesecor.com>
2016-04-10 18:13:06 -0400

    Did Poisson
    Poisson

--- tripAdvisor.R
+++ tripAdvisor.R
@@ -1,4 +1,17 @@
 # Trip Advisor
 
-taData <- read.csv(file= "https://www.dropbox.com/s/rgysz2oiljgux3p/taAllCities.csv?dl=0",head =TRUE, sep=",")
+taData <- read.csv(file= "C:/Users/Jake/OneDrive/School Work/Senior Spring/Data Analytics/Data Sets/taAllCities.csv", head=TRUE, sep=",")
 
+taData <- read.csv(file= "C:\\Users\\Elias\\Documents\\BC Junior Year\\Analytics & Business Intelligence\\taAllCities.csv",head =TRUE, sep=",")
+
+head(taData)
+
+# poisson to see significance of independent variables
+poisson <- glm(rating ~ service + vibe +  desert + bathroom + drink + cost + music + location + parking + lunch + breakfast + dinner + totalReviewsOfReviewer + avgHelpfulnessOfReviewer, family="poisson", data =taData)
+summary(poisson)
+
+
+# marginal effects of independent variables
+library(mfx)
+
+poissonmfx(rating ~ service + vibe +  desert + bathroom + drink + cost + music + location + parking + lunch + breakfast + dinner + totalReviewsOfReviewer + avgHelpfulnessOfReviewer, data =taData)
